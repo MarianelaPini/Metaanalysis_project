bbbc304bb614033ec99a4364c9acbbac720fda3b
MarianelaPini <marianelapini@usp.br>
2023-12-26 20:16:09 -0300

    UPdating script

--- R/01_F_graphics.R
+++ R/01_F_graphics.R
@@ -1,71 +1,4 @@
 #Grafico grau de degradação e tipo de controle
-#########################################
-#juntando os dois gráficos no mesmo painel
-X11(width = 12, height = 6)
-layout(matrix(c(1, 2),ncol=2, nrow=1), width=c(6, 6))
-layout.show(2) 
-par (mar = c(5, 12, 4, 3.5), bty = "n" )
-plot(x = NULL, y = NULL, xlim = c(-7, 6), ylim = c(0.5, 5), 
-     type = "n", yaxt = "n", xlab = "Effect Size (Hedges´d)", 
-     ylab = "",  main = " A) STUDY AREA")
-#linhas guias e eixo#
-#funcao abline, lty para definir o tipo de linha, 2 e tracejada#
-abline (v = 0, lty = 2) 
-#legendas
-axis(side = 2, at = c(1,2,3,4),
-     labels=c("without tree degraded (3)",
-              "without tree intermediate (4)","with tree degraded (8)",
-              "with tree intermediate (12)"),las =2,tick = FALSE,lty="blank")
-# without tree degraded
-points(x = 2.8107, y = 1, pch = 19) # pch: tipo de simbolo
-points(x = 0.5989, y = 1, pch = "|", cex = 1.2)
-points(x = 5.0225, y = 1, pch = "|", cex = 1.2)
-segments(x0 = 0.5989, y0 = 1, x1 = 5.0225, y1 = 1) # um segmento
-# without tree intermediate
-points(x = -3.8738, y = 2, pch = 19) # pch: tipo de simbolo
-points(x = -6.9631, y = 2, pch = "|", cex = 1.2)
-points(x = -0.7845, y = 2, pch = "|", cex = 1.2)
-segments(x0 = -6.9631, y0 = 2, x1 = -0.7845, y1 = 2) # um segmento
-#with tree degraded
-points(x = -1.6131, y = 3, pch = 19) # pch: tipo de simbolo
-points(x = -4.2164, y = 3, pch = "|", cex = 1.2)
-points(x = 0.9903, y = 3, pch = "|", cex = 1.2)
-segments(x0 = -4.2164, y0 = 3, x1 = 0.9903, y1 = 3) # um segmento
-#with tree intermediate
-points(x = 2.0111, y = 4, pch = 19) # pch: tipo de simbolo
-points(x = -1.5155, y = 4, pch = "|", cex = 1.2)
-points(x = 5.5377, y = 4, pch = "|", cex = 1.2)
-segments(x0 = -1.5155, y0 = 4, x1 = 5.5377, y1 = 4) # um segmento
-#segundo grafico
-par (mar = c(5, 12, 4, 3.5), bty = "n" )
-plot(x = NULL, y = NULL, xlim = c(-7, 6), ylim = c(0.5, 5), 
-     type = "n", yaxt = "n", xlab = "Effect Size (Hedges´d)", 
-     ylab = "",  main = "B) ORIGIN OF CONTROL TREES")
-#linhas guias e eixo#
-#funcao abline, lty para definir o tipo de linha, 2 e tracejada#
-abline (v = 0, lty = 2) 
-#legendas
-axis(side = 2, at = c(1.5,2.5,3.5,4.5), 
-     labels=c("native trees (4)","exotic trees (8)", 
-              "native and exotic trees (8)"),"compared to:", 
-     las =2,tick = FALSE,lty="blank")
-# native trees
-points(x = -2.5017, y = 1.5, pch = 19) # pch: tipo de simbolo
-points(x = -0.0672, y = 1.5, pch = "|", cex = 1.2)
-points(x = -4.9362, y = 1.5, pch = "|", cex = 1.2)
-segments(x0 = -4.9362, y0 = 1.5, x1 = -0.0672, y1 = 1.5) # um segmento
-# exotic trees
-points(x = 1.2119, y = 2.5, pch = 19) # pch: tipo de simbolo
-points(x = -0.2794, y = 2.5, pch = "|", cex = 1.2)
-points(x = 2.7031, y = 2.5, pch = "|", cex = 1.2)
-segments(x0 = -0.2794, y0 = 2.5, x1 = 2.7031, y1 = 2.5) # um segmento
-# native and exotic trees
-points(x = -1.5872, y = 3.5, pch = 19) # pch: tipo de simbolo
-points(x = -3.5955, y = 3.5, pch = "|", cex = 1.2)
-points(x = 0.4212, y = 3.5, pch = "|", cex = 1.2)
-segments(x0 = -3.5955, y0 = 3.5, x1 = 0.4212, y1 = 3.5) # um segmento
-savePlot("metaGraf2.png", type = "png")
-help(axis)
 #########################################################
 #Graphic without titles and with model without intercept
 #########################################################
@@ -80,10 +13,10 @@ plot(x = NULL, y = NULL, xlim = c(-7, 6), ylim = c(0.5, 5),
 #funcao abline, lty para definir o tipo de linha, 2 e tracejada#
 abline (v = 0, lty = 2) 
 #legendas
-axis(side = 2, at = c(1,2,3,4),
+axis(side = 2, at = c(1,2,3,4,4.5),
      labels=c("without tree degraded (3)",
               "without tree intermediate (4)","with tree degraded (8)",
-              "with tree intermediate (12)"),las =2,tick = FALSE,lty="blank")
+              "with tree intermediate (12)","Control:"),las =2,tick = FALSE,lty="blank")
 # without tree degraded
 points(x = 2.8107, y = 1, pch = 19) # pch: tipo de simbolo
 points(x = 0.5989, y = 1, pch = "|", cex = 1.2)
@@ -116,7 +49,7 @@ abline (v = 0, lty = 2)
 #legendas
 axis(side = 2, at = c(1.5,2.5,3.5,4.5), 
      labels=c("native trees (4)","exotic trees (8)", 
-              "native and exotic trees (8)","compared to:"), 
+              "native and exotic trees (8)","Compared to:"), 
      las =2,tick = FALSE,lty="blank")
 # native trees
 points(x = -1.2898, y = 1.5, pch = 19) # pch: tipo de simbolo
@@ -137,26 +70,6 @@ text(x=-7,y=5,"b)")
 ##############
 #save graphic#
 savePlot("metaGraf0.png", type = "png")
-#Installing ORchard
-#install.packages("pacman")
-rm(list = ls())
-devtools::install_github("daniel1noble/orchaRd", ref = "main", force = TRUE)
-pacman::p_load(devtools, tidyverse, metafor, patchwork, R.rsp, orchaRd, emmeans, ape, phytools, flextable)
-data(english)
-english
-# We need to calculate the effect sizes, in this case d
-english <- escalc(measure = "SMD", n1i = NStartControl, sd1i = SD_C, m1i = MeanC, n2i = NStartExpt, sd2i = SD_E, m2i = MeanE, 
-                  var.names=c("SMD","vSMD"),
-                  data = english)
-
-english_MA <- rma.mv(yi = SMD, V = vSMD, random = list( ~ 1 | StudyNo, ~ 1 | EffectID), data = english)
-summary(english_MA)
-model_results <- orchaRd::mod_results(english_MA, mod = "1", at = NULL,  group = "StudyNo")
-model_results
-orchaRd::orchard_plot(english_MA, mod="1", group = "StudyNo", xlab = "Standardised mean difference", 
-                      transfm = "none") 
-# a caterpillar plot (not a caterpillars plot)
-orchaRd::caterpillars(model_results, mod="1", xlab = "Standardised mean difference") 
 #funnel plots
 X11(width = 12, height = 6)
 layout(matrix(c(1,2),ncol=2, nrow=1), width=c(6, 6))
